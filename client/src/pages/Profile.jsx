f43981e35f1e557305643a7add3149fe4adb9803
mudasir <mudasirbhat602@gmail.com>
2024-04-24 22:48:53 +0530

    add signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  // signOutUserStart,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 // import { Link } from 'react-router-dom';
@@ -102,6 +104,20 @@ function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+  const handleSignOut=async()=>{
+    try {
+     dispatch(signOutUserStart())
+      const res=await fetch("api/auth/signout")
+      const data=await res.json()
+      if(data.success===false){
+        dispatch(signOutUserFailure(data.message))
+        return
+      }
+      dispatch(signOutUserSuccess(data))
+    } catch (error) {
+dispatch(signOutUserFailure(data.message))      
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -169,12 +185,13 @@ function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully" : ""}
       </p>
+      
     </div>
   );
 }
